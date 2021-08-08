572cbf9386492bbde4f938307f7c2fa815a908d8
Aviral Aman <aviralaman25@gmail.com>
2021-08-09 02:56:06 +0530

    hello

---
+++ Basic-Config-Microservices-Balanced/user-authentication/src/main/java/in/programming/userauthentication/controller/UserController.java
@@ -0,0 +1,16 @@
+package in.programming.userauthentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/status")
+    public String welcoe(){
+        return "hello world";
+    }
+
+}
